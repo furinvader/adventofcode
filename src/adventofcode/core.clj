9f49ef01dc26826281ebb649f7d4dd038e8e51e0
Alex Dik <mail@alexdik.de>
2023-12-21 04:32:56 +0100

    Add ability to solve puzzles via `lein run`

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -1,7 +1,16 @@
 (ns adventofcode.core
   (:gen-class))
 
+(defn read-puzzle-input
+  [year day puzzle]
+  (try (slurp (str "resources/puzzle_inputs/year_" year
+                   "/day_" day
+                   "_puzzle_" puzzle
+                   ".txt"))
+       (catch java.io.FileNotFoundException e
+         (str "No puzzle input found for year:" year " day:" day " puzzle:" puzzle))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [[year day puzzle] args]
+    (println (read-puzzle-input year day puzzle))))
