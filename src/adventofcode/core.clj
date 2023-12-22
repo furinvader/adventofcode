8534bc0bdd7fd253e2137c2bfbf43693b99f5cda
Alex Dik <mail@alexdik.de>
2023-12-22 10:30:43 +0100

    Solve 2023 day 1 puzzle 1

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -1,5 +1,6 @@
 (ns adventofcode.core
-  (:gen-class))
+  (:gen-class)
+  (:require [adventofcode.year-2023.day-1-puzzle-1 :as y2023d1p1]))
 
 (defn read-puzzle-input
   [year day puzzle]
@@ -8,9 +9,13 @@
                    "_puzzle_" puzzle
                    ".txt"))
        (catch java.io.FileNotFoundException e
-         (str "No puzzle input found for year:" year " day:" day " puzzle:" puzzle))))
+         (println "No puzzle input found for year:" year " day:" day " puzzle:" puzzle))))
+
+(def solved-puzzles {"2023" {"1" {"1" y2023d1p1/solve-puzzle}}})
 
 (defn -main
   [& args]
-  (let [[year day puzzle] args]
-    (println (read-puzzle-input year day puzzle))))
+  (let [[year day puzzle] args
+        puzzle-input (read-puzzle-input year day puzzle)
+        puzzle-solver (get-in solved-puzzles [year day puzzle])]
+    (println (puzzle-solver puzzle-input))))
