1eeb18987c78f5ff249767f340792fee2c788991
Alex Dik <mail@alexdik.de>
2023-12-30 20:15:05 +0100

    Improve 2023 day 3 performance

--- src/adventofcode/year_2023/day_3/puzzle_1.clj
+++ src/adventofcode/year_2023/day_3/puzzle_1.clj
@@ -4,59 +4,71 @@
   (:require [clojure.set :as set]
             [clojure.string :as str]))
 
-(defn parse-positions [pattern input]
-  (as-> input i
-    (str/split-lines i)
-    (map-indexed (fn [index line]
-                   (as-> (re-seq pattern line) l
-                     (map rest l)
-                     (reduce (fn [result [gap target]]
-                               (let [prev (last result)
-                                     offset (if prev 1 0)
-                                     prev-x (last (:x prev '(0)))
-                                     start-x (+ prev-x (count gap) offset)
-                                     end-x (+ start-x (count target))]
-                                 (conj result
-                                       {:target target
-                                        :x (vec (range start-x end-x))
-                                        :y [index]})))
-                             [] l)))
-                 i)
-    (flatten i)))
+(defn pair-with-previous
+  ([f]
+   (fn [rf]
+     (let [prev (volatile! ::none)]
+       (fn
+         ([] (rf))
+         ([result] (rf result))
+         ([result input]
+          (let [prior @prev
+                next (if (= prior ::none)
+                       (f [input])
+                       (f [input prior]))]
+            (vreset! prev next)
+            (rf result next)))))))
+  ([f coll] (sequence (pair-with-previous f) coll)))
 
-(defn parse-part-numbers [input]
-  (->> input
+(defn parse-positions [pattern lines]
+  (->> lines
+       (map #(re-seq pattern %1))
+       (map #(map rest %))
+       (map-indexed #(map (fn [[gap target]] {:gap gap :target target :y [%1]}) %2))
+       (map #(pair-with-previous
+              (fn [[cur prev]]
+                (let [offset (if prev 1 0)
+                      prev-x (last (:x prev '(0)))
+                      start-x (+ prev-x (count (:gap cur)) offset)
+                      end-x (+ start-x (count (:target cur)))]
+                  (assoc cur :x (vec (range start-x end-x)))))
+              %))
+       (flatten)))
+
+(defn parse-part-numbers [lines]
+  (->> lines
        (parse-positions #"(.*?)(\d+)")
        (map #(assoc % :value (Integer/parseInt (:target %))))
        (map #(update % :x set))
        (map #(update % :y set))))
 
-(defn parse-parts [input]
-  (->> input
+(def stretch 
+  (juxt #(- % 1) identity #(+ 1 %)))
+
+(defn parse-parts [lines]
+  (->> lines
        (parse-positions #"(.*?)([^\.0-9])")
-       (map #(let [[x] (:x %)
-                   [y] (:y %)]
-               (-> %
-                   (assoc :x [(- x 1) x (+ x 1)])
-                   (assoc :y [(- y 1) y (+ y 1)]))))
+       (map #(update % :x (comp stretch first)))
+       (map #(update % :y (comp stretch first)))
        (map #(update % :x set))
        (map #(update % :y set))))
 
 (defn assign-adjacent-numbers [parts numbers]
-  (as-> parts s
-      (map (fn [part]
-             (let [x (:x part)
-                   y (:y part)
-                   adjacent (filter #(and
-                              (< 0 (count (set/intersection x (:x %))))
-                              (< 0 (count (set/intersection y (:y %))))) numbers)]
-               (assoc part :adjacent (map :value adjacent))))
-           s)
-      ))
+  (let [numbers (group-by (comp first :y) numbers)]
+    (map (fn [part]
+           (assoc part :adjacent
+                  (map :value (->> part
+                                   (:y)
+                                   (select-keys numbers)
+                                   (vals)
+                                   (flatten)
+                                   (filter #(< 0 (count (set/intersection (:x part) (:x %)))))))))
+         parts)))
 
 (defn get-parts-with-adjacent-numbers [input]
-  (let [numbers (parse-part-numbers input)
-        parts (parse-parts input)]
+  (let [lines (str/split-lines input)
+        numbers (parse-part-numbers lines)
+        parts (parse-parts lines)]
     (assign-adjacent-numbers parts numbers)))
 
 (defn solve-puzzle [input]
